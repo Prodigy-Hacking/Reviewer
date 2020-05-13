bf2f6cfd50504a83e03db631d60c27ccc2d4ce1a
KryptoCrash <kryptocrashmh@gmail.com>
2020-05-13 12:40:26 -0400

    Auto Warn and Support System 1.0.2

--- commands/hackinfo.js
+++ commands/hackinfo.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, channel) => {
     let supportEmbed = new Discord.RichEmbed()
         .setAuthor('Reviewer -', bot.avatarURL)
         .setTitle("SUPPORT")
-        .setDescription(`All of our hacks are listed on our github, which can be accessed in the <#710159575738744985> channel. You can also watch this video for a full tutorial on how to hack: https://www.youtube.com/watch?v=DMwDyV9mmsM`)
+        .setDescription(`All of our hacks are listed on our github, which can be accessed in the <#683847137511079959> channel. You can also watch this video for a full tutorial on how to hack: https://www.youtube.com/watch?v=DMwDyV9mmsM`)
         .setColor(colors.info)
     channel.sendEmbed(supportEmbed)
 }
