63b9dec8c3d4f950385d59a149c195f50f5e81d2
BoredFish <49598984+BoredFishRE@users.noreply.github.com>
2020-10-14 09:47:44 -0400

    readme update and smol ready.js update

--- events/ready.js
+++ events/ready.js
@@ -7,13 +7,13 @@ exports.run = async (bot, msg) => {
     bot.user.setPresence({
         status: "online",
         game: {
-            name: `${botsettings.prefix}help for ${bot.users.size} users.`
+            name: `${botsettings.prefix}help for ${bot.users.cache.size} users.`
         }
     });
     setInterval(() => bot.user.setPresence({
         status: "online",
         game: {
-            name: `${botsettings.prefix}help for ${bot.users.size} users.`
+            name: `${botsettings.prefix}help for ${bot.users.cache.size} users.`
         }
     }), 60 * 60 * 1000); 
 };
